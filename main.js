ccf9c4ae5da8c5dc5d7b17a3b89616676aa2f9af
Markus <markholden1989@googlemail.com>
2018-04-11 13:44:15 +0100

    Animate rain

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 		let COLOR, Rain, NUM_RAIN, canvas, ctx, rain, drawRain, i, range, ANGLE, LENGTH_RAIN;
 		
 		// How rainy is it?
-		NUM_RAIN = 200;
+		NUM_RAIN = 1;
 		
 		// Angle of rain
 		ANGLE = 45;
@@ -28,6 +28,7 @@
 		
 		// Draw the rain
 		drawRain = function(xStart, yStart, xEnd, yEnd){
+			ctx.beginPath();
 			ctx.strokeStyle = `rgba(${COLOR[0]}, ${COLOR[1]}, ${COLOR[2]}, 0.8)`;
 			ctx.moveTo(xStart,yStart);
 			ctx.lineTo(xEnd,yEnd);
@@ -41,7 +42,18 @@
 				this.xEnd = this.xStart + (LENGTH_RAIN * Math.sin(-ANGLE / (Math.PI/180)));
 				this.yEnd = this.yStart + (LENGTH_RAIN * Math.cos(-ANGLE / (Math.PI/180)));
 			}
+			fall() {
+				this.xStart = this.xStart + 1;
+				this.yStart = this.yStart + 1;
+				this.xEnd = this.xStart + (LENGTH_RAIN * Math.sin(-ANGLE / (Math.PI/180)));
+				this.yEnd = this.yStart + (LENGTH_RAIN * Math.cos(-ANGLE / (Math.PI/180)));
+				console.log('xStart' + this.xStart);
+				console.log('yStart' + this.yStart);
+				console.log('xEnd' + this.xEnd);
+				console.log('yEnd' + this.yEnd);
+			}
 			draw() {
+				this.fall();
 				return drawRain(this.xStart, this.yStart, this.xEnd, this.yEnd);
 			}
 		}
@@ -55,12 +67,19 @@
 			}
 			return rainDrops;
 		})();
-		
-		drops = [];
-		for ( i = 1, len = rain.length; i < len; i++){
-			r = rain[i];
-			drops.push(r.draw());
+			
+		run = function(){
+			let r, i, len, drops;
+			requestAnimationFrame(run);
+			ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
+			drops = [];
+			for ( i = 0, len = rain.length; i < len; i++){
+				r = rain[i];
+				drops.push(r.draw());
+			}
+			return drops;
 		}
+		run();
 		
 	}
 
